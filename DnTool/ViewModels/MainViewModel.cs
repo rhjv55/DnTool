c384281baad590fc8db4f01b4aa1ae5005e98834
rhjv55 <654215540@qq.com>
2015-06-03 09:08:32 +0800

    11

--- DnTool/ViewModels/MainViewModel.cs
+++ DnTool/ViewModels/MainViewModel.cs
@@ -21,7 +21,7 @@ namespace DnTool.ViewModels
         private DmPlugin dm = new DmPlugin();
         public MainViewModel()
         {
-            this.CurrentPoint = new Point("0","0","0");
+           
             this.CloseTimer = new RelayCommand(() =>
             {
                 timer.Stop();
@@ -41,12 +41,16 @@ namespace DnTool.ViewModels
                     // = address.ToString("F2");
                     //this.CurrentPoint.Y = address.ToString("F2");
                     //this.CurrentPoint.Z = address.ToString("F2");
-                    int address=dm.ReadInt(201936, "[1221740]+a5c",0);
-                    X=(BitConverter.ToSingle(BitConverter.GetBytes(address), 0)).ToString("F2");
-                    float v = dm.ReadFloat(201936, "[1221740]+a5c");
+                    //int address=dm.ReadInt(201936, "[1221740]+a5c",0);
+                    //X=(BitConverter.ToSingle(BitConverter.GetBytes(address), 0)).ToString("F2");
+                    //float v = dm.ReadFloat(201936, "[1221740]+a5c");
 
-                    Logger.Debug(Convert.ToString(address, 16));
-                    Logger.Debug(v.ToString());
+                    //Logger.Debug(Convert.ToString(address, 16));
+                    //Logger.Debug(v.ToString());
+                    this.X = new Random().Next(100).ToString();
+                    this.Y = new Random().Next(10000).ToString();
+                    this.Z = new Random().Next(1000).ToString();
+                    dm.Delay(200);
                 };
             timer.Interval = TimeSpan.FromMilliseconds(1000);
             timer.Start();
@@ -68,14 +72,38 @@ namespace DnTool.ViewModels
             MemoryHelper.WriteMemoryValue(baseAdd, processName, value);
         }
         public RelayCommand CloseTimer { get; set; }
-        public Point CurrentPoint { get; set; }
+
+        private string _y;
+
+        public string Y
+        {
+            get { return _y; }
+            set
+            {
+                _y = value;
+                this.OnPropertyChanged("Y");
+            }
+        }
+
+        private string _z;
+
+        public string Z
+        {
+            get { return _z; }
+            set
+            {
+                _z = value;
+                this.OnPropertyChanged("Z");
+            }
+        }
+        
         private string _x;
 
         public string X
         {
             get { return _x; }
             set { _x = value;
-            OnPropertyChanged("X");
+            this.OnPropertyChanged("X");
             }
         }
     }
