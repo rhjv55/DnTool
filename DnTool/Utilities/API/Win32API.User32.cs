6dc6a48ba2e5c34eab2ce94296318c61b9e3bd4c
rhjv55 <654215540@qq.com>
2015-10-13 17:30:48 +0800

    0000

--- DnTool/Utilities/API/Win32API.User32.cs
+++ DnTool/Utilities/API/Win32API.User32.cs
@@ -13,5 +13,44 @@ namespace DnTool.Utilities.API
         [return: MarshalAs(UnmanagedType.Bool)]
        public static extern bool GetCursorPos(out Point lpPoint);
         // Or use System.Drawing.Point (Forms only)
+
+        /// <summary>
+        /// 该函数将一个消息放入到与指定窗口创建的线程相联系消息队列里，不等待线程处理消息就返回
+        /// </summary>
+        /// <param name="hWnd">其窗口程序接收消息的窗口的句柄</param>
+        /// <param name="Msg">指定被寄送的消息</param>
+        /// <param name="wParam">指定附加的消息特定的信息</param>
+        /// <param name="lParam">指定附加的消息特定的信息</param>
+        /// <returns></returns>
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool PostMessage(int hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [Flags]
+        public enum MouseEventFlags : uint
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010,
+            WHEEL = 0x00000800,
+            XDOWN = 0x00000080,
+            XUP = 0x00000100
+        }
+        //Use the values of this enum for the 'dwData' parameter
+        //to specify an X button when using MouseEventFlags.XDOWN or
+        //MouseEventFlags.XUP for the dwFlags parameter.
+        public enum MouseEventDataXButtons : uint
+        {
+            XBUTTON1 = 0x00000001,
+            XBUTTON2 = 0x00000002
+        }
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData,
+          int dwExtraInfo);
     }
 }
