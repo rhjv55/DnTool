d08f03c39df68f3998d4cf28c3ae7e10d4611593
rhjv55 <654215540@qq.com>
2015-10-16 17:04:15 +0800

    3333

--- HPlugin/Main/Impl/KeypadByPostMsg.cs
+++ HPlugin/Main/Impl/KeypadByPostMsg.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace IPlugin.Main
 {
-    public class KeypadByMsg:IKeypad
+    public class KeypadByPostMsg:IKeypad
     {
+        private int _hwnd;
+        public KeypadByPostMsg(int hwnd)
+        {
+            _hwnd = hwnd;
+        }
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool PostMessage(int hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+
         public bool KeyDown(VirtualKeyCode k)
         {
-            throw new NotImplementedException();
+            return PostMessage(_hwnd,0,(IntPtr)0,(IntPtr)0);
         }
 
         public bool KeyUp(VirtualKeyCode k)
